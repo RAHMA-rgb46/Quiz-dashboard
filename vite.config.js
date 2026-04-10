acc2c22b51826c514c9001269b48e19d42489f35
rahmargb46 <ramzy.mommy@gmail.com>
2026-04-10 13:47:40 +0000

    Fix Github Pages build base path

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/quiz-dashboard/',
 })
 
 
